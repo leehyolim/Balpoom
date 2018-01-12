fcd4189efc7882d77fdfa60d4e6eefbeb6306ba9
ndgndg91 <ndgndg91@gmail.com>
2018-01-12 12:40:24 +0900

    mybatis

--- src/main/java/com/balpoom/view/MemberController.java
+++ src/main/java/com/balpoom/view/MemberController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.balpoom.member.MemberService;
 import com.balpoom.member.MemberVO;
-import com.balpoom.member.Impl.MemberDAO;
 
 @Controller
 public class MemberController {
